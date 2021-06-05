04cfaa96985b17b07fd8691851fe21a3979ce834
Jesswin W Varghese <jesswinw@gmail.com>
2021-06-06 01:46:23 +0530

    changed some styles

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,4 +1,4 @@
 <nav class="text-center py-6 text-blue-900 ">
-	<a href="/" class="cursor-pointer hover:underline mx-4">Home</a>
-	<a href="/about" class="cursor-pointer hover:underline">About</a>
+	<a href="/" class="cursor-pointer py-2 px-6 rounded-md hover:bg-blue-200 mx-4">Home</a>
+	<a href="/about" class="cursor-pointer py-2 px-6 rounded-md hover:bg-blue-200">About</a>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <svelte:head>
-	<title>Home Page</title>
+	<title>Home</title>
 </svelte:head>
 
-<h1 class="text-4xl font-bold text-blue-700 text-center">Welcome to SvelteKit</h1>
+<h1 class="text-4xl font-bold text-blue-700 text-center">Svelte pokedex</h1>
 <div class="grid grid-cols-1 md:grid-cols-3 gap-3 p-16">
 	{#each $pokemon as pokeman (pokeman.id)}
 		<PokeCard {pokeman} />
